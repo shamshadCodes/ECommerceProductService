0892639aa5f760822e6e1bb48385a5cdaec76427
shamshad <lookatitonce@gmail.com>
2023-11-12 13:37:34 +0530

    added DB configurations and tested table creation

--- src/main/java/com/scaler/ECommerceProductService/model/Product.java
+++ src/main/java/com/scaler/ECommerceProductService/model/Product.java
@@ -1,11 +1,16 @@
 package com.scaler.ECommerceProductService.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
     private String id;
     private String name;
     private String description;
