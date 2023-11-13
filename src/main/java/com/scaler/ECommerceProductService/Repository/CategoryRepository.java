53b99fdc7bae0a3b73755304dba56f2fcd676977
shamshad <lookatitonce@gmail.com>
2023-11-13 10:33:31 +0530

    added repository interfaces

--- src/main/java/com/scaler/ECommerceProductService/Repository/CategoryRepository.java
+++ src/main/java/com/scaler/ECommerceProductService/Repository/CategoryRepository.java
@@ -1,11 +1,11 @@
 package com.scaler.ECommerceProductService.Repository;
 
-import com.scaler.ECommerceProductService.model.Product;
+import com.scaler.ECommerceProductService.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
 }
