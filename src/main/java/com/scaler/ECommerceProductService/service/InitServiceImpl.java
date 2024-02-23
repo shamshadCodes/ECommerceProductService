deb876277b90bcafae5009463b8fd6f13b49c61f
shamshad <lookatitonce@gmail.com>
2024-02-23 18:19:22 +0530

    Updating hard-coded data

--- src/main/java/com/scaler/ECommerceProductService/service/InitServiceImpl.java
+++ src/main/java/com/scaler/ECommerceProductService/service/InitServiceImpl.java
@@ -4,7 +4,6 @@ import com.scaler.ECommerceProductService.Repository.CategoryRepository;
 import com.scaler.ECommerceProductService.Repository.OrderRepository;
 import com.scaler.ECommerceProductService.Repository.PriceRepository;
 import com.scaler.ECommerceProductService.Repository.ProductRepository;
-import com.scaler.ECommerceProductService.controller.ProductController;
 import com.scaler.ECommerceProductService.model.Category;
 import com.scaler.ECommerceProductService.model.Order;
 import com.scaler.ECommerceProductService.model.Price;
@@ -57,46 +56,46 @@ public class InitServiceImpl implements InitService{
         pricePhone = priceRepository.save(pricePhone);
         priceLaptop = priceRepository.save(priceLaptop);
         priceHeadphone = priceRepository.save(priceHeadphone);
-        priceWatch = priceRepository.save(priceWatch);
+        priceRepository.save(priceWatch);
 
         Product iPhone = new Product();
-        iPhone.setName("IPhone");
+        iPhone.setTitle("IPhone");
         iPhone.setImage("http://www.images.com/some-image");
         iPhone.setDescription("This is an IPhone produced by Apple.");
         iPhone.setPrice(pricePhone);
         iPhone.setCategory(electronics);
-        iPhone = productRepository.save(iPhone);
+        productRepository.save(iPhone);
 
         Product laptop = new Product();
-        laptop.setName("Macbook Air");
+        laptop.setTitle("Macbook Air");
         laptop.setImage("http://www.images.com/some-image");
         laptop.setDescription("This is a macbook produced by Apple.");
         laptop.setPrice(priceLaptop);
         laptop.setCategory(electronics);
-        laptop = productRepository.save(laptop);
+        productRepository.save(laptop);
 
         Product headphone = new Product();
-        headphone.setName("Audio-Technica Model H");
+        headphone.setTitle("Audio-Technica Model H");
         headphone.setImage("http://www.images.com/some-image");
         headphone.setDescription("This is a headphone produced by Audio-Technica.");
         headphone.setPrice(priceHeadphone);
         headphone.setCategory(electronics);
-        headphone = productRepository.save(headphone);
+        productRepository.save(headphone);
 
         Product iWatch = new Product();
-        iWatch.setName("IWatch");
+        iWatch.setTitle("IWatch");
         iWatch.setImage("http://www.images.com/some-image");
         iWatch.setDescription("This is a watch produced by Apple.");
         iWatch.setPrice(priceWatch);
         iWatch.setCategory(electronics);
-        iWatch = productRepository.save(iWatch);
+        productRepository.save(iWatch);
 
         Order order1 = new Order();
         order1.setProducts(List.of(iWatch, iPhone));
-        order1 = orderRepository.save(order1);
+        orderRepository.save(order1);
 
         Order order2 = new Order();
         order2.setProducts(List.of(laptop, headphone));
-        order2 = orderRepository.save(order2);
+        orderRepository.save(order2);
     }
 }
