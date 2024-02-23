e3dd3abe4b6b4c363096b3ffbe4b160bf18979f3
shamshad <lookatitonce@gmail.com>
2024-02-24 00:16:03 +0530

    updated product mappers

--- src/main/java/com/scaler/ECommerceProductService/mapper/ProductMapper.java
+++ src/main/java/com/scaler/ECommerceProductService/mapper/ProductMapper.java
@@ -1,14 +1,16 @@
 package com.scaler.ECommerceProductService.mapper;
 
-import com.scaler.ECommerceProductService.dto.FakeStoreProductRequestDTO;
-import com.scaler.ECommerceProductService.dto.FakeStoreProductResponseDTO;
-import com.scaler.ECommerceProductService.dto.ProductRequestDTO;
-import com.scaler.ECommerceProductService.dto.ProductResponseDTO;
+import com.scaler.ECommerceProductService.dto.*;
+import com.scaler.ECommerceProductService.model.Category;
+import com.scaler.ECommerceProductService.model.Price;
+import com.scaler.ECommerceProductService.model.Product;
+
+import java.util.List;
 
 public class ProductMapper {
     public static ProductResponseDTO fakeStoreProductResponseToProductResponse(FakeStoreProductResponseDTO fakeStoreProductResponseDTO){
         ProductResponseDTO productResponseDTO = new ProductResponseDTO();
-        productResponseDTO.setId(fakeStoreProductResponseDTO.getId());
+        productResponseDTO.setId(String.valueOf(fakeStoreProductResponseDTO.getId()));
         productResponseDTO.setTitle(fakeStoreProductResponseDTO.getTitle());
         productResponseDTO.setPrice(fakeStoreProductResponseDTO.getPrice());
         productResponseDTO.setCategory(fakeStoreProductResponseDTO.getCategory());
@@ -27,5 +29,46 @@ public class ProductMapper {
         fakeStoreProductRequestDTO.setDescription(productRequestDTO.getDescription());
 
         return fakeStoreProductRequestDTO;
+    }
+
+    public static Product fakeStoreProductToProduct(FakeStoreProductResponseDTO fakeStoreProduct){
+        Product product = new Product();
+        Price price = new Price();
+        Category category = new Category();
+
+        price.setPrice(fakeStoreProduct.getPrice());
+
+        category.setCategoryName(fakeStoreProduct.getCategory());
+
+        product.setId(String.valueOf(fakeStoreProduct.getId()));
+        product.setTitle(fakeStoreProduct.getTitle());
+        product.setImage(fakeStoreProduct.getImage());
+        product.setDescription(fakeStoreProduct.getDescription());
+        product.setPrice(price);
+        product.setCategory(category);
+
+        return product;
+    }
+
+    public static  ProductResponseDTO productToProductResponseDTO(Product product){
+        ProductResponseDTO productResponseDTO = new ProductResponseDTO();
+        productResponseDTO.setId(product.getId());
+        productResponseDTO.setTitle(product.getTitle());
+        productResponseDTO.setDescription(product.getDescription());
+        productResponseDTO.setImage(product.getImage());
+        productResponseDTO.setCategory(product.getCategory().getCategoryName());
+        productResponseDTO.setPrice(product.getPrice().getPrice());
+
+        return productResponseDTO;
+    }
+
+    public static ProductListResponseDTO productListToProductListResponseDTO(List<Product> productList){
+        ProductListResponseDTO productListResponseDTO = new ProductListResponseDTO();
+        for(Product product: productList){
+            productListResponseDTO.getProductList()
+                    .add(productToProductResponseDTO(product));
+        }
+
+        return productListResponseDTO;
     }
 }
