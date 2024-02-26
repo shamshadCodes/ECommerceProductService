c1510258d73ef71664f5da91a9594d4acd67cfcc
shamshad <lookatitonce@gmail.com>
2024-02-26 18:04:07 +0530

    fixing warnings

--- src/main/java/com/scaler/ECommerceProductService/Repository/ProductRepository.java
+++ src/main/java/com/scaler/ECommerceProductService/Repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.scaler.ECommerceProductService.Repository;
 
 import com.scaler.ECommerceProductService.model.Product;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
     @Override
+    @NonNull
     List<Product> findAll();
 }
