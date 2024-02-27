6d2f6a4d8649fc0fba8a27b3327472eb68d1637c
shamshad <lookatitonce@gmail.com>
2024-02-27 07:28:20 +0530

    validation checks for ProductRequestDTO

--- src/main/java/com/scaler/ECommerceProductService/dto/ProductRequestDTO.java
+++ src/main/java/com/scaler/ECommerceProductService/dto/ProductRequestDTO.java
@@ -1,16 +1,31 @@
 package com.scaler.ECommerceProductService.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ProductRequestDTO {
+    @NotBlank(message = "Title cannot be blank.")
+    @Size(min = 3)
     private String title;
-    private String description;
+
+    @Size(min = 5, message = "Description must be at least 5 characters long.")
+    private String description = "";
+
+    @NotBlank(message = "Category cannot be blank.")
     private String category;
+
     private String image;
+
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0.")
     private double price;
-    private String currencyCode;
+
+    @Size(min = 3, max = 3, message = "Currency code must be 3 characters long.")
+    private String currencyCode = "INR";
+
+    @Min(value = 0, message = "Discount percentage cannot be negative.")
+    @Max(value = 100, message = "Discount percentage cannot exceed 100%.")
     private double discountPercentage;
 }
