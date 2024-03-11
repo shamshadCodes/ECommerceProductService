68d81c0716e061abe1b762d7fad5f16ebcf6d1e3
shamshad <lookatitonce@gmail.com>
2024-03-11 10:32:42 +0530

    updated settings file

--- src/main/java/com/scaler/ECommerceProductService/model/BaseModel.java
+++ src/main/java/com/scaler/ECommerceProductService/model/BaseModel.java
@@ -3,7 +3,6 @@ package com.scaler.ECommerceProductService.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
